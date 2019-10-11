1ee37a1a9d704b8d160442e5da5d1daa5b4eb223
Patrick Joseph Concepcion <patrickjosephconcepcion@gmail.com>
2019-10-11 23:05:39 +0800

    Rotating & Shooting player (#11)

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -9,23 +9,32 @@ public class Laser : MonoBehaviour
 
     // Start is called before the first frame update
     private void Start()
-    {
-        
+    {   
     }
 
     // Update is called once per frame
     private void Update()
-    {
+    { 
         transform.Translate(Vector3.up * _speed * Time.deltaTime);
 
-        if ( transform.position.y >= 6.75f)
+        if (transform.position.y >= 6.4f || transform.position.y <= -6.4f)
         {
-            if( transform.parent != null)
-            {
-                Destroy(transform.parent.gameObject);
-            }
-
-            Destroy(this.gameObject);
+            DestroyLaser();
         }
+
+        if (transform.position.x <= -11.5f || transform.position.x >= 11.1f)
+        {
+            DestroyLaser();
+        }
+    }
+
+    private void DestroyLaser()
+    {
+        if( transform.parent != null)
+        {
+            Destroy(transform.parent.gameObject);
+        }
+
+        Destroy(this.gameObject);
     }
 }
